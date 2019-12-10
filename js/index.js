50cbf043aef3c88fe301e831a3a7295f73267aee
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2019-12-10 15:40:56 -0700

    added HTML and CSS

---
+++ js/index.js
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Store Elements in variables
+// var highScore = $('#high-scores');
+// var timer = $('#timer');
+// var start = $('#start');
+// var startBtn = $('#start-btn');
+// var quiz = $('#quiz');
+// var quizTitle = $('#title');
+// var choices = $('choices');
+// var choiceA =  $('#a');
+// var choiceB =  $('#b');
+// var choiceC =  $('#c');
+// var answer = $('#answer');
+// var score = 0;
+
+// var questions = [
+//     {
+//       title: "Commonly used data types DO NOT include:",
+//       choices: ["strings", "booleans", "alerts", "numbers"],
+//       answer: "alerts"
+//     },
+//     {
+//       title: "The condition in an if / else statement is enclosed within ____.",
+//       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+//       answer: "parentheses"
+//     },
+//     ///etc.
+//   ];
+
+
+// $(document).ready(function(){
+   
+   
+//     $("button").click(function(){
+//       $("button").hide();
+
+//       for(var i = 0; i < questions.length; i++) {
+//           quiz.show();
+//         var quizQuestions = questions[i];
+//         quiz.append($(`<div class="card">
+//         <h3">${quizQuestions.title}</h3>
+//         <div class="card-body">
+//             <button>${quizQuestions.choices}</button>
+//             <button id="button-navigate-" type="button" class="btn btn-primary">Find</button>
+//         </div>
+//         </div>`));
+//       }
+      
+//     });
+
+//   });
+
+
